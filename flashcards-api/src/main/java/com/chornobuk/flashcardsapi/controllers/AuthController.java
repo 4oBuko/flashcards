fb62e6d30134c54e6c506d2ebd1714c0fbde5fa9
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-10-20 21:23:07 +0300

    registration implementation

--- flashcards-api/src/main/java/com/chornobuk/flashcardsapi/controllers/AuthController.java
+++ flashcards-api/src/main/java/com/chornobuk/flashcardsapi/controllers/AuthController.java
@@ -1,18 +1,33 @@
 package com.chornobuk.flashcardsapi.controllers;
 
+import com.chornobuk.flashcardsapi.entities.User;
+import com.chornobuk.flashcardsapi.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
+@AllArgsConstructor
 public class AuthController {
+    private UserService userService;
+
     @PostMapping("/login")
     public ResponseEntity<String> loginUser() {
-        return ResponseEntity.ok("todo") ;
+        return ResponseEntity.ok("todo");
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<String> refreshAccesstoken() {
-        return ResponseEntity.ok("todo") ;
+        return ResponseEntity.ok("todo");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerNewUser(@RequestBody User newUser) {
+        User registeredUser = userService.registerNewUser(newUser);
+        if(registeredUser != null) {
+            return ResponseEntity.ok("Registration successful!");
+        }
+        return ResponseEntity.badRequest().body("Registration failed");
     }
 }
--- flashcards-api/src/main/java/com/chornobuk/flashcardsapi/services/UserService.java
+++ flashcards-api/src/main/java/com/chornobuk/flashcardsapi/services/UserService.java
@@ -9,11 +9,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Service
 @AllArgsConstructor
 public class UserService implements UserDetailsService {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+
     public User getById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
@@ -22,5 +26,13 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findUserByEmail(email);
         return user;
+    }
+
+    public User registerNewUser(User newUser) {
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+        newUser.setRegistrationDate(LocalDate.now());
+        newUser.setConfirmed(true);//todo: change on false after implementing email verification
+        newUser = userRepository.save(newUser);
+        return newUser;
     }
 }
