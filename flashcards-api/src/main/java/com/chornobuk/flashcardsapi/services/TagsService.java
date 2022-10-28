80815333bd4b8b311e57dd2eb27cbe09e11f9daf
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-10-28 20:57:06 +0300

    create tag service

---
+++ flashcards-api/src/main/java/com/chornobuk/flashcardsapi/services/TagsService.java
@@ -0,0 +1,25 @@
+package com.chornobuk.flashcardsapi.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.chornobuk.flashcardsapi.entities.Tag;
+import com.chornobuk.flashcardsapi.entities.User;
+import com.chornobuk.flashcardsapi.repositories.TagRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class TagsService {
+    private TagRepository tagRepository;
+
+    public List<Tag> getTagsByUser(User user) {
+        return tagRepository.findAllByUser(user);
+    }
+
+    public Tag getTagById(long tagId) {
+        return tagRepository.findById(tagId).orElse(null);
+    }
+}
