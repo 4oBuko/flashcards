f2ccb776872d1438236465d0bb2910004947214f
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-10-28 20:56:16 +0300

    endpoints for getting tag by id and tags by user

--- flashcards-api/src/main/java/com/chornobuk/flashcardsapi/controllers/TagsController.java
+++ flashcards-api/src/main/java/com/chornobuk/flashcardsapi/controllers/TagsController.java
@@ -1,33 +1,52 @@
 package com.chornobuk.flashcardsapi.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.chornobuk.flashcardsapi.entities.Tag;
+import com.chornobuk.flashcardsapi.entities.User;
+import com.chornobuk.flashcardsapi.services.TagsService;
+import com.chornobuk.flashcardsapi.services.UserService;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("tags")
+@AllArgsConstructor
 public class TagsController {
+
+    private TagsService tagsService;
+
+    private UserService userService;
+
     @GetMapping("/{userId}")
-    public ResponseEntity<String> getUserTags() {
-        return ResponseEntity.ok("todo") ;
+    public ResponseEntity<List<Tag>> getUserTags(@PathVariable long userId) {
+        User user = userService.getById(userId);
+        if(user == null) {
+            return ResponseEntity.badRequest().body(null);
+        }
+        return ResponseEntity.ok(tagsService.getTagsByUser(user));
     }
 
     @GetMapping("tag/{tagId}")
-    public ResponseEntity<String> getTagById() {
-        return ResponseEntity.ok("todo") ;
+    public ResponseEntity<Tag> getTagById(@PathVariable long tagId) {
+        return ResponseEntity.ok(tagsService.getTagById(tagId));
     }
 
     @PostMapping()
-    public ResponseEntity<String> addNewTag(){
-        return ResponseEntity.ok("todo") ;
+    public ResponseEntity<String> addNewTag() {
+        return ResponseEntity.ok("todo");
     }
 
     @PutMapping("/{tagId}")
     public ResponseEntity<String> updateTagById() {
-        return ResponseEntity.ok("todo") ;
+        return ResponseEntity.ok("todo");
     }
 
     @DeleteMapping("/{tagId}")
     public ResponseEntity<String> deleteTagById() {
-        return ResponseEntity.ok("todo") ;
+        return ResponseEntity.ok("todo");
     }
 }
--- flashcards-api/src/main/java/com/chornobuk/flashcardsapi/repositories/TagRepository.java
+++ flashcards-api/src/main/java/com/chornobuk/flashcardsapi/repositories/TagRepository.java
@@ -1,7 +1,12 @@
 package com.chornobuk.flashcardsapi.repositories;
 
 import com.chornobuk.flashcardsapi.entities.Tag;
+import com.chornobuk.flashcardsapi.entities.User;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface TagRepository extends CrudRepository<Tag, Long> {
+    List<Tag> findAllByUser(User user);
 }
