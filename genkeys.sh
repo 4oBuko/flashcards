771947239af30ebd4ff137753627f51322d772e0
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-11-29 22:09:41 +0200

    add shell script for generating ssl keys

---
+++ genkeys.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+P=$1/keys
+
+#create directory for keys
+mkdir -p $P
+
+# create rsa key pair
+openssl genrsa -out $P/keypair.pem 2048
+
+# extract public key
+openssl rsa -in $P/keypair.pem -pubout -out $P/public.pem
+
+# create private key in PKCS#8 format
+openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in $P/keypair.pem -out $P/private.pem
