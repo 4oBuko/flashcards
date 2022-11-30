f642209045ee3811d673a684755f8b7783ad6516
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-11-30 19:00:46 +0200

    change parameters and add idea how write the method

--- flashcards-api/src/main/java/com/chornobuk/flashcardsapi/utils/ConfirmationLetterBuilder.java
+++ flashcards-api/src/main/java/com/chornobuk/flashcardsapi/utils/ConfirmationLetterBuilder.java
@@ -1,13 +1,17 @@
 package com.chornobuk.flashcardsapi.utils;
 
 import com.chornobuk.flashcardsapi.entities.User;
+import com.chornobuk.flashcardsapi.entities.VerificationToken;
 
 public class ConfirmationLetterBuilder {
-    public static String generateRegistrationLetter(User user) {
+    public static String generateRegistrationLetter(VerificationToken token) {
         return null;//todo
     }
 
-    public static String generateEmailUpdateLetter(User user) {
+    public static String generateEmailUpdateLetter(VerificationToken token) {
+        // todo: for confirmation letter I can create an html template
+        // and read it from this method and set 
+        // (html letter will be pretty but create a text letter is easier and faster)
         return null;//todo
     }
 }
