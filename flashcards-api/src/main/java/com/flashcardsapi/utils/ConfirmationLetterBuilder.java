c4428a096f8f93039e1535a36b6bc8289f951b97
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-12-11 20:47:58 +0200

    add url generator

--- flashcards-api/src/main/java/com/flashcardsapi/utils/ConfirmationLetterBuilder.java
+++ flashcards-api/src/main/java/com/flashcardsapi/utils/ConfirmationLetterBuilder.java
@@ -1,14 +1,19 @@
 package com.flashcardsapi.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.flashcardsapi.entities.VerificationToken;
 
 public class ConfirmationLetterBuilder {
+
+    // @Value("${domain.name}")//todo:add domain
+    private final static String DOMAIN ="http://localhost:8080/register/confirm";
 
     public static String generateRegistrationLetter(VerificationToken token) {
         String message = """
                 Hi %s, you registered on %s. Activate your account by the link bellow:
                 %s
-                """.formatted(token.getUser().getNickname(), "site", token.getToken());
+                """.formatted(token.getUser().getNickname(), "site", generateURL(token.getToken()));
         return message;
     }
 
@@ -16,8 +21,12 @@ public class ConfirmationLetterBuilder {
         String message = """
                 Hi %s, your email was updated to %s. To set new email, follow the link bellow:
                 %s
-                """.formatted(token.getUser().getNickname(), token.getUser().getEmail(), token.getToken());
+                """.formatted(token.getUser().getNickname(), token.getUser().getEmail(), generateURL(token.getToken()));
         return message;
         // todo: write tests
+    }
+
+    public static String generateURL(String token) {
+        return DOMAIN + "?token=" + token;
     }
 }
