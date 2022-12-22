1593a755b68c9d45b49ab5401c8f275f1d64a867
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-12-22 20:52:28 +0200

    add function for user registration
    create file and method to register new users with description

---
+++ client/src/api/Registration.js
@@ -0,0 +1,6 @@
+import API_URLS from "../config/api-routes";
+
+export function register(nickname, email, password) {
+    // make request to api with data for registration
+    // return message from api
+}
