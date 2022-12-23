b7f369a7d78a25d4c67381bdd66f039bb4c57e6d
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-12-23 23:38:43 +0200

    create method sending request with body

--- client/src/api/Registration.js
+++ client/src/api/Registration.js
@@ -1,6 +1,21 @@
-import API_URLS from "../config/api-routes";
+import { API_URLS } from "../config/api-routes.js";
 
 export function register(nickname, email, password) {
-    // make request to api with data for registration
-    // return message from api
+  // make request to api with data for registration
+  // return message from api
+  let registrationInfo = {
+    nickname: nickname,
+    password: password,
+    email: email,
+  };
+  const request = new Request(API_URLS.USER_REGISTER, {
+    method: "POST",
+    body: JSON.stringify(registrationInfo),
+  });
+
+  fetch(request).then((data) => {
+    console.log(data);
+  });
 }
+
+register("user1", "user1", "user1");
