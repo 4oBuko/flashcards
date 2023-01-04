e2719a28fb97702c7d2636229123861c15ac2f24
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-01-04 17:43:53 +0200

    add function for refreshing access token

--- client/src/api/Authentication.js
+++ client/src/api/Authentication.js
@@ -1,8 +1,13 @@
 import { API_URLS } from "../config/api-routes.js";
 
-export async function logIn(email, password) {
+export async function login(email, password) {
     // todo try to login user with email and password
     // if authentication successful get access token and
     // put refresh token in http only cookies
     // 
 }
+
+export async function refresh() {
+    // send request to get new access token
+}
+
