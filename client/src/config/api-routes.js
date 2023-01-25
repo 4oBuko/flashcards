3802f9b2878725b3249e2e867da6bcc3d6094f2c
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-01-25 20:33:21 +0200

    rename endpoints constants

--- client/src/config/api-routes.js
+++ client/src/config/api-routes.js
@@ -2,19 +2,21 @@ export const API_URL = "http://localhost:8080";
 // todo: how to set api change api url for dev and production 
 // TODO rename constants
 export const API_URLS = {
-  ACCOUNT_LOGIN: `${API_URL}/auth/login`,
-  ACCOUNT_REFRESH_TOKEN: `${API_URL}/auth/refresh`,
+  LOGIN: `${API_URL}/auth/login`,
+  REFRESH_TOKEN: `${API_URL}/auth/refresh`,
   CHECK_NICKNAME: `${API_URL}/users/nickname/:nickname`,
-  GET_COLORS : `${API_URL}/colors`,
-  GET_LANGUAGES : `${API_URL}/languages`,
+
+  COLORS_GET : `${API_URL}/colors`,
+  
+  LANGUAGES_GET : `${API_URL}/languages`,
 
   USER_REGISTER: `${API_URL}/register`,
-  GET_USER: `${API_URL}/users/:id`,
+  USER_GET: `${API_URL}/users/:id`,
   USER_CHANGE_EMAIL: `${API_URL}/users/:id/email`,
   USER_CHANGE_NICKNAME: `${API_URL}/users/:id/nickname`,
   USER_CHANGE_PASSWORD: `${API_URL}/users/:id/password`,
-  USER_SETS: `${API_URL}/users/:id/sets`,
-  USER_TAGS: `${API_URL}/users/:id/tags`,
+  USER_SETS_GET: `${API_URL}/users/:id/sets`,
+  USER_TAGS_GET: `${API_URL}/users/:id/tags`,
 
   TAG_GET : `${API_URL}/tags/:id`,
   TAG_CREATE : `${API_URL}/tags/`,
