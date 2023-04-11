b97881418e39c6b8629e46f0be586d802fe53214
Kostiantyn Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-04-11 10:36:52 +0300

    add register view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,14 @@
 import Login from "@/views/Login.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
+import Register from "@/views/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", name: "home", component: Home },
     { path: "/login", name: "login", component: Login },
+    {path: "/register", name: "register", component: Register}
   ],
 });
 
---
+++ client/src/views/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <input v-model="email" type="email" name="email" id="email" />
+    <br />
+    <input
+      v-model="password"
+      type="password"
+      maxlength="50"
+      name="password"
+      id="password"
+    />
+    <br />
+    <input
+      v-model="nickname"
+      type="text"
+      minlength="5"
+      maxlength="50"
+      name="password"
+      id="password"
+    />
+    <br />
+    <button v-on:click="registerUser">Register</button>
+  </div>
+</template>
+
+<script>
+import { registerNewUser } from "@/api/FlashcardsApi";
+
+export default {
+  name: "Register",
+  data() {
+    return {
+      nickname: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    registerUser() {
+      const response = registerNewUser(
+        this.nickname,
+        this.email,
+        this.password
+      );
+      console.log(response);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
